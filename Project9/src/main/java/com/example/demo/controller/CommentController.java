dc94d764557b7589fd5cb5e22b13cb693c0de38c
lyuds <lyuds9094@gmail.com>
2024-10-08 11:33:13 +0900

    10.08

--- Project9/src/main/java/com/example/demo/controller/CommentController.java
+++ Project9/src/main/java/com/example/demo/controller/CommentController.java
@@ -3,7 +3,9 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,4 +29,30 @@ public class CommentController {
 		return commentlist;
 	}
 	
+	//등록 메소드
+	@PostMapping("/register")
+	public int register(CommentDTO dto) {
+		
+		//스프링 시큐리티를 처리하기 전까지는 임시 아이디 사용
+		dto.setWriter("user1");
+		
+		//테이블에 댓글 저장
+		int newNo = service.register(dto);
+		
+		return newNo;
+		
+	}
+	
+	//댓글 삭제 메소드
+	@DeleteMapping("/remove")
+	public Boolean remove(@RequestParam(name = "commentNo") int commentNo) {
+		// 댓글 삭제
+		boolean result = service.remove(commentNo);
+		// 처리결과 반환
+		return result;
+	}
+	
+	
+	
+
 }
